f6bf48eb83b77756315c9176c78fcb00f22d99ed
praddesh <pradeep.acky@gmail.com>
2023-08-22 18:35:57 +0530

    Initial project files

---
+++ src/test/java/com/springboot/weathercacheservice/WeatherCacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.weathercacheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCacheServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
